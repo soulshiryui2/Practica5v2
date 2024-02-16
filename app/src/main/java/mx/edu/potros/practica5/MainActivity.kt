8d72712d63e71bed7416fb6ea98a0fc68acade21
ChristianPartida <christian.partida175627@potros.itson.edu.mx>
2024-02-15 19:17:02 -0700

    Primera verson de la practica5 de la clase de apps moviles del dia 15 ya funciona correctamente la navegacion de las ventanas y el diseno se hizo igual a las especificaciones

---
+++ app/src/main/java/mx/edu/potros/practica5/MainActivity.kt
@@ -0,0 +1,18 @@
+package mx.edu.potros.practica5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button: Button = findViewById(R.id.btn_get_started)
+        button.setOnClickListener {
+            var intent: Intent=Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
